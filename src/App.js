c4782b67f11ac4145b905bf1913ba3b95831fa46
Ivan583 <ivanych2293@gmail.com>
2022-08-04 12:56:04 +0300

    Создание компонента рейса

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import './App.css';
+import FlightItem from './components/FlightItem';
 
 function App() {
   return (
     <div className="App">
       <h1>My React Application</h1>
-      <div className='weighting'>
-
-        <div className='column'>
-           <span>"Bear"</span> 
-        </div>
-
-        <div className='column'>
-           <span>2900</span> 
-        </div>
-
-      </div>
+      <FlightItem />
     </div>
   );
 }
---
+++ src/components/FlightItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const FlightItem = () => {
+
+
+    return (
+        <div className='weighting'>
+
+        <div className='column'>
+           <span>"Bear"</span> 
+        </div>
+
+        <div className='column'>
+           <span>2900</span> 
+        </div>
+
+      </div>
+    );
+};
+
+export default FlightItem;
