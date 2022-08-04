2efee8be2bd0ec4d1b89625bcbbd83474a84f63f
Ivan583 <ivanych2293@gmail.com>
2022-08-04 12:28:27 +0300

    Добавление рейса

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
   return (
     <div className="App">
       <h1>My React Application</h1>
+      <div className='weighting'>
+
+        <div className='column'>
+           <span>"Bear"</span> 
+        </div>
+
+        <div className='column'>
+           <span>2900</span> 
+        </div>
+
+      </div>
     </div>
   );
 }
