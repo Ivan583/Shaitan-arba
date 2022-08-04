b366ed379b3bcdc283ca145e16ac292e79d5f020
Ivan583 <ivanych2293@gmail.com>
2022-08-04 15:19:04 +0300

    Подключение кнопки

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import FlightList from './components/FlightList';
+import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
 
 function App() {
@@ -10,6 +11,18 @@ function App() {
     { driver: "Wolf", weight: 2700 },
     { driver: "Bear", weight: 2800 },
   ]);
+
+  const [driver, setDriver] = useState('');
+  const [weight, setWeight] = useState(0);
+
+  const addNewFlight = e => {
+    e.preventDefault();
+    const newFlight = {
+      driver,
+      weight: parseInt(weight)
+    }
+    setTrucks([...trucks, newFlight]);
+  }
 
   function countSummary(arr) {
     const drivers = [...arr].map(el => el.driver);
@@ -38,9 +51,18 @@ function App() {
   return (
     <div className="App">
       <form>
-        <MyInput type="text" placeholder="driver" />
-        <MyInput type="number" />
-        <button>Добавить рейс</button>
+        <MyInput 
+          value={driver}
+          onChange={e => setDriver(e.target.value)}
+          type="text" 
+          placeholder="driver" />
+
+        <MyInput
+          value={weight}
+          onChange={e => setWeight(e.target.value)}
+          type="number" />
+
+        <MyButton onClick={addNewFlight}>Добавить рейс</MyButton>
       </form>
 
       <FlightList group={groupSummary} total={totalSummary} />
