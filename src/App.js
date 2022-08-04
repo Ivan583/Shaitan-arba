0288846b0e43c5ce64fbc5728d075f077d3d1fae
Ivan583 <ivanych2293@gmail.com>
2022-08-04 13:10:25 +0300

    Ввод начальных данных

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import FlightList from './components/FlightList';
 
 
 function App() {
+
+  const [trucks, setTrucks] = useState([
+    { driver: "Bear", weight: 2900 },
+    { driver: "Wolf", weight: 2700 },
+    { driver: "Bear", weight: 2800 },
+  ]);
+
   return (
     <div className="App">
       <FlightList />
